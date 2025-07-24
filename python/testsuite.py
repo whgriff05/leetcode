64c3aab9f9780cc1ff79b5d54fb37da4986c55b0
Will Griffin <willhgriffin@icloud.com>
2025-07-24 17:16:06 -0400

    Added linked list features

--- python/testsuite.py
+++ python/testsuite.py
@@ -4,6 +4,9 @@ class ListNode:
         self.val = val
         self.nxt = nxt
 
+    def __repr__(self):
+        return f"<ListNode ({self.val}) -> {self.nxt}>"
+
 # Helper functions
 def list_to_linked_list(l):
     prev = None
@@ -23,9 +26,27 @@ def print_linked_list(l):
 
 # Test functions
 def assert_eq(a, b):
+    # Comparing Linked Lists
+    if isinstance(a, ListNode) and isinstance(b, ListNode):
+        alist = []
+        blist = []
+        while a != None:
+            alist.append(a.val)
+            a = a.nxt
+
+        while b != None:
+            blist.append(b.val)
+            b = b.nxt
+
+        a = alist
+        b = blist
+
+    # Comparing Lists
     if isinstance(a, list) and isinstance(b, list):
         a.sort()
         b.sort()
+
+        
     if a == b:
         return True
     else:
